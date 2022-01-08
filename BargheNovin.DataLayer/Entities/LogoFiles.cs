7096ac7bb1046c5abab30b8dbf69ae06c5ca5dab
amirreza265 <amir64499@gmail.com>
2022-01-08 10:12:22 +0330

    add logo

---
+++ BargheNovin.DataLayer/Entities/LogoFiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BargheNovin.DataLayer.Entities
+{
+    public class LogoFiles
+    {
+        public int LogoId { get; set; }
+        public string MainLogo { get; set; }
+        public string FooterLogo { get; set; }
+        public string SmallLogo { get; set; }
+    }
+}
